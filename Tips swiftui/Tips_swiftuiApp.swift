de18210f1e5ada465b4d064170f29109b0527fe1
hosam <hosamelmalt@gmail.com>
2021-03-13 10:10:32 +0200

    Initial Commit

---
+++ Tips swiftui/Tips_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_swiftuiApp.swift
+//  Tips swiftui
+//
+//  Created by hosam on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct Tips_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
